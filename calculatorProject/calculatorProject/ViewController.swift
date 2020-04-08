bddf0e63d6bb20a48464a2b74d956785c011863e
karyRmartinez <karymartinez@pursuit.org>
2020-04-08 14:08:01 -0400

    UIView setup

---
+++ calculatorProject/calculatorProject/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  calculatorProject
+//
+//  Created by Kary Martinez on 4/8/20.
+//  Copyright © 2020 Kary Martinez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    enum Operations {
+        case add
+        case subtract
+        case multiply
+        case divide
+    }
+    
+    lazy var BackgroundView: UIView = {
+        let view = UIView()
+        return view
+    }()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
+ 
